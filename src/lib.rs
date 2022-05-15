0a6013be6da4f057c2ebe37b63efee2707a6b9d9
Demir Yerli <mrquantumoff@protonmail.com>
2022-05-15 11:24:58 +0300

    add base encryption

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
-pub mod encryption {
+pub mod keytools
+{
+    use colored::*;
     use rand::{self, random};
+
+    pub fn check_equality(key1: String, key2: String) -> bool {
+        if key1==key2 {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 
     fn rand_string(length: i32) -> String {
         (0..length).map(|_| (0x20u8 + (random::<f32>() * 96.0) as u8) as char).collect()
@@ -7,7 +19,7 @@ pub mod encryption {
 
     pub fn check_if_key_is_valid(key: &str) -> bool {
         if key.len() != 45 {
-            println!("{}, Key isn't valid", key.len());
+            println!("{}{}{}","Key isn't valid: ".bright_red(), key.len()," characters long.".bright_red());
             return false;
         }
         let mut nums: i32 = 0;
@@ -42,4 +54,29 @@ pub mod encryption {
         return key;
     }
 }
+
+pub mod encryption {
+    use colored::*;
+    use magic_crypt::{new_magic_crypt, MagicCryptTrait};
+    pub fn encrypt_string(key: String, text: String) -> String {
+        let mc = new_magic_crypt!(key, 256);
+        let encrypted = mc.encrypt_str_to_base64(text);
+        return encrypted;
+    }
+    pub fn decrypt_string(key: String, encrypted_data: String) -> String {
+        let mc = new_magic_crypt!(key, 256);
+        let decrypted = mc.decrypt_base64_to_string(encrypted_data);
+        match decrypted {
+            Err(e) => {
+                println!("Detailed Error: {}", e.to_string().bright_red());
+                return "!!!Invalid Key!!!".to_string();
+            }
+            Ok(s) => {
+                return s;
+            }
+        }
+        //return decrypted;
+    }
+}
+
 pub mod msgservices {}
