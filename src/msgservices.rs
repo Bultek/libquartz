25e399f4aa95f9061b3c9da796d46ff022ee5406
Demir Yerli <mrquantumoff@protonmail.com>
2022-05-15 15:49:27 +0300

    add sender name

--- src/msgservices.rs
+++ src/msgservices.rs
@@ -2,14 +2,16 @@ use reqwest::*;
 use std::{collections::HashMap};
 
 #[allow(dead_code)]
-pub async fn send_msg(server: String, msg: String, key: String, contact: String) -> bool {
+pub async fn send_msg(server: String, msg: String, key: String, contact: String, sender: String) -> bool {
     // Returns true if everything went well
-    let message = crate::encryption::encrypt_string(key, msg);
+    let message = crate::encryption::encrypt_string(key.clone(), msg);
     let client = Client::new();
+    let author = crate::encryption::encrypt_string(key.clone(), sender);
     // Create a proper .JSON object
     let mut data = HashMap::new();
     data.insert("message", message);
     data.insert("contact", contact);
+    data.insert("sender", author);
     // Send the message
     let res = client.post(server)
         .json(&data)
