8e3fd6ca20218cf7b46d7e5e978252c20bea67dc
Demir Yerli <mrquantumoff@protonmail.com>
2022-05-16 19:01:11 +0300

    move quartz to tests of libquarz

---
+++ tests/src/main.rs
@@ -0,0 +1,54 @@
+//use colored::*;
+use libquartz::*;
+use std::{env, fs::*};
+use futures::executor::block_on;
+use tokio;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Check if ~/.qkey exists
+    #[allow(deprecated)]
+    let home_dir = env::home_dir().unwrap();
+    let qkey_path = home_dir.join(".qkey");
+    if metadata(&qkey_path).is_err() {
+        // Create ~/.qkey
+        let key = keytools::gen_key();
+        std::fs::write(&qkey_path, key).unwrap();
+    }
+
+    let qkey = qkey_path.into_os_string().into_string().unwrap();
+    let key = std::fs::read_to_string(qkey).unwrap();
+    if keytools::check_if_key_is_valid(&key) == false {
+        Err("Key isn't valid".to_string())?;
+    }
+    let k = key.clone();
+    let msg = encryption::encrypt_string(String::from(&key),"Hello World!".to_string());
+    let result = msgservices::send_msg(
+        "http://127.0.0.1:8000/incoming".to_string(),
+        msg,
+        k.clone(),
+        "test".to_string(),
+        "tester".to_string(),
+    );
+    let r = block_on(result);
+    println!("{}", r);
+    let result = msgservices::send_msg(
+        "http://127.0.0.1:8000/incoming".to_string(),
+        "Hello World!".to_string(),
+        k.clone(),
+        "Hello".to_string(),
+        "World".to_string(),
+    );
+    let b = block_on(result);
+    println!("{}", b);
+    // Recieve messages and print
+    let a = msgservices::get_msgs_encrypted(
+        "http://127.0.0.1:8000/messages".to_string(),
+        encryption::encrypt_string(k.clone(), "Hello".to_string()),
+    );
+    let o = block_on(a);
+    let d = msgservices::decrypt_msgs(o, k.clone());
+    for i in 0..d.messages.len() {
+        println!("{} - {}",d.senders[i], d.messages[i]);
+    }
+    Ok(())
+}
